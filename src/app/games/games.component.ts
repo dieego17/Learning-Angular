5d75fa52c15201283377fd1e1062e567910eeec3
dieego17 <diegorubio1704.drs@gmail.com>
2024-03-11 13:14:52 +0100

    creating components and states

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h3>Juegos favoritos de {{ username }}</h3>
+    <ul>
+      <!-- for para recorrer games y le indicamos el identificador unico de cada juego -->
+      @for (game of games; track game.id) {
+        <li>{{ game.name }}</li>
+      }
+    </ul>
+  `,
+  styles: ``
+})
+export class GamesComponent {
+  @Input() username = '';
+  games = [
+    {
+      id: 1,
+      name: 'Uncharted 4'
+    },
+    {
+      id: 2,
+      name: 'The Last of Us'
+    },
+    {
+      id: 3,
+      name: 'God of War'
+    }
+    
+  ];
+}
